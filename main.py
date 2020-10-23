dffad3030543b13c2f40716259db77c43a38af1c
baiztheprogrammer <baizakmadzhinov@gmail.com>
2020-10-23 12:33:58 +0600

    new test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-number_list = [1, 2, 7, 4, 10, 6, 3]
-k = 5
-c = 12
-number_list.insert(k,c)
-print(number_list)
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+test_list = number_list
+test_list.reverse()
+
+print(test_list)
+
